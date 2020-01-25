8ecf4a502fef5e5fd5c138c50520e71563d189c4
CiprianaM <cipriana2001@gmail.com>
2020-01-25 16:50:33 +0100

    test for pre-commit hooks

--- db.js
+++ db.js
@@ -1,9 +1,12 @@
 const mongoose = require ('mongoose');
-const mongoDB = 'mongodb://localhost/historyDB'; //this should be the link from mlab
+// const mongoDB = 'mongodb://localhost/historyDB'; //this should be the link from mlab
+const mongoDB = "mongodb://Cipriana:Floricescu1.@cluster0-j53v9.mongodb.net/prod"
 
 mongoose.connect(mongoDB, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }, (error)=> console.log('Successfully connected to DB!'));
 module.exports = {mongoose};
+
+
 

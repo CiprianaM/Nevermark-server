d519b28bad643760a34ad8bdbade10628cee575b
CiprianaM <cipriana2001@gmail.com>
2020-01-27 12:06:17 +0100

    initial elasticsearch instalation and connection

---
+++ elasticDb.js
@@ -0,0 +1,26 @@
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'trace',
+  apiVersion: '7.2', // use the same version of your Elasticsearch instance
+});
+
+client.ping({
+  // ping usually has a 3000ms timeout
+  requestTimeout: 1000
+}, function (error) {
+  if (error) {
+    console.trace('elasticsearch cluster is down!');
+  } else {
+    console.log('All is well');
+  }
+});
+
+// client.search({
+//   q: 'pants'o
+// }).then(function (body) {
+//   var hits = body.hits.hits;
+// }, function (error) {
+//   console.trace(error.message);
+// });
+

9f26931fe99cf3631761474380f29dd95d15b023
davstern <hello@davidstern.me>
2020-01-28 20:19:10 +0100

    fix: uncorrect evaluation of integers

--- utils/checkObjProps.js
+++ utils/checkObjProps.js
@@ -3,25 +3,26 @@ const checkObjProps = (obj,checkRules)=> {
   for (rule of checkRules) {
     if (!obj[rule.prop]) {
       if (rule.required) {
-        error.push(`missing required field : "${rule.prop}"`);
+        error.push(`missing required field [${rule.prop}]`);
       }
       continue;
     }
-    const val = obj['prop'];
+    const val = obj[rule.prop];
+
     if (!rule.type) {
       continue;
     }
 
     if (rule.type === 'integer') {
-      if (Math.floor(parseInt(val)) !== val) {
-        error.push(`expected type : "${rule.type}" for field : "${rule.prop}"`);
-      } else if (typeof val !== rule.prop) {
-        error.push(`expected type : "${rule.type}" for field : "${rule.prop}"`);
+      if (Math.floor(parseInt(val)).toString() !== val.toString()) {
+        error.push(`expected type [${rule.type}] for field [${rule.prop}]`);
       }
+    } else if (typeof val !== typeof rule.prop) {
+      error.push(`expected type [${rule.type}] for field [${rule.prop}]`);
     }
   }
 
-  return error ? {error} : {valid : true};
+  return error.length ? {error} : {valid : true};
 };
 
 module.exports = checkObjProps;

fba96d1d777fe510fd261bba1c695ae9dc4e09c9
davstern <hello@davidstern.me>
2020-01-28 14:06:30 +0100

    chore: add dotenv for db parameters

--- db.js
+++ db.js
@@ -1,15 +1,16 @@
-require('dotenv').config();
 const mongoose = require ('mongoose');
-// const mongoDB = 'mongodb://localhost/historyDB'; //this is a local DB used for tests
-const mongoDB = "mongodb+srv://cluster0-j53v9.mongodb.net/prod"  //standard connection string format
+const { DB_HOST,DB_PORT,DB } = process.env;
+try {
+  mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB}`,{
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+    useCreateIndex : true,
+    // user : process.env.DB_USER,
+    // pass : process.env.DB_PASS
+  });
+  console.log('Successfully connected to db');
+} catch (e) {
+  console.log('Error trying go connect to db : ' + e);
+}
 
-mongoose.connect(mongoDB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  user: process.env.DB_USER,
-  pass: process.env.DB_PASS
-}, (error)=> console.log('Successfully connected to DB!'));
-module.exports = {mongoose};
-
-
-
+module.exports = mongoose;

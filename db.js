8c3a9f1086f43921d69e974113478d37033a0e2c
davstern <hello@davidstern.me>
2020-01-29 15:11:34 +0100

    fix: connect was logging success message in catch scenario

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 const mongoose = require ('mongoose');
 const { DB_HOST,DB_PORT,DB } = process.env;
+
 try {
   mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB}`,{
     useNewUrlParser : true,
     useUnifiedTopology : true,
     useCreateIndex : true,
-    // user : process.env.DB_USER,
-    // pass : process.env.DB_PASS
+    user : process.env.DB_USER,
+    pass : process.env.DB_PASS
   });
   console.log('Successfully connected to db');
 } catch (e) {

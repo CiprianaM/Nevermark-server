f151721be6c9a90a7e51836730706d77079f9d86
davstern <hello@davidstern.me>
2020-01-29 23:20:53 +0100

    feat: add autocreation of collections before first request. (ACID transactions only works if collection created)

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const basename = path.basename(__filename);
+const db = require('../db');
 
 const model = {};
 
@@ -8,8 +9,9 @@ fs
   .readdirSync(__dirname)
   .filter((file) => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
   .forEach((file) => {
-
-    model[file.split('.')[0]] = require(path.join(__dirname,file));
+    const colName = file.split('.')[0];
+    model[colName] = require(path.join(__dirname,file));
+    model[colName].init();
   });
 
 module.exports = model;

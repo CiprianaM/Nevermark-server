6ad16cea17e795acb1a3cd8efdf79b1617df568d
davstern <hello@davidstern.me>
2020-01-30 13:08:04 +0100

    chore: fix merge conflict

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 const router = require ('express').Router();
-const addNewRecord = require('./elasticControllers/addOne');
+const actions = require('./controllers');
 const retrieveMultipleTextSearch = require('./elasticControllers/searchMultipleText');
 const retrieveMultipleTitleSearch = require('./elasticControllers/searchMultipleTitle');
 const getAll = require('./elasticControllers/searchAll');
@@ -7,6 +7,6 @@ const getAll = require('./elasticControllers/searchAll');
 router.get('',retrieveMultipleTextSearch);
 router.get('/all',getAll);
 router.get('/title',retrieveMultipleTitleSearch);
-router.post('',addNewRecord);
+router.post('',actions.insertUserVisit);
 
 module.exports = router;

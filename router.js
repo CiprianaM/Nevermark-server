dcaaffcd14e9d0ba93734ec91bebf06b664b1005
davstern <hello@davidstern.me>
2020-01-30 13:23:34 +0100

    fix: correct the routes

--- router.js
+++ router.js
@@ -1,12 +1,8 @@
 const router = require ('express').Router();
 const actions = require('./controllers');
-const retrieveMultipleTextSearch = require('./elasticControllers/searchMultipleText');
-const retrieveMultipleTitleSearch = require('./elasticControllers/searchMultipleTitle');
-const getAll = require('./elasticControllers/searchAll');
+const getAll = require('./elasticControllers/search');
 
-router.get('',retrieveMultipleTextSearch);
-router.get('/all',getAll);
-router.get('/title',retrieveMultipleTitleSearch);
+router.get('',getAll);
 router.post('',actions.insertUserVisit);
 
 module.exports = router;

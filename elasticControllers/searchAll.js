94bc3c726b7e5b6b2481fd5de5311727d45b140c
davstern <hello@davidstern.me>
2020-01-30 13:22:59 +0100

    fix: total hit parameter returns correct value

--- elasticControllers/searchAll.js
+++ elasticControllers/searchAll.js
@@ -3,46 +3,47 @@ const client = require('../elasticDb');
 
 // ------------------ expected req object start ---------------------------//
 const receivedReq = {
-  body: {
-    pageNum: 1
+  body : {
+    pageNum : 1
   }
-}
+};
 // ------------------ expected req object stop ---------------------------//
 
-const retrieveAll = async (req, res) => {
+const retrieveAll = async (req,res) => {
   let pageNum = 0;
-  if(req.body.pageNum !== undefined) pageNum = req.body.pageNum - 1;
+  if (req.body.pageNum !== undefined) pageNum = req.body.pageNum - 1;
   const pageSize = 20;
   const searchedText = req.body.pageText;
   try {
     const result = await client.search({
-      index: 'history',
-      body: {
-        size: pageSize,
-        from: pageNum*pageSize,
-        query: {
-          match_all: {}
+      index : 'history',
+      body : {
+        size : pageSize,
+        from : pageNum * pageSize,
+        query : {
+          match_all : {}
         }
       }
-    })
-    const totalPages = Math.ceil(result.body.hits.total.value/20);
+    });
+
+    const totalPages = Math.ceil(result.body.hits.total / 20);
     console.log(totalPages);
     const response = {
-      hits: result.body.hits.hits.length,
-      totalPageNum: totalPages,
-      results: []
+      hits : result.body.hits.hits.length,
+      totalPageNum : totalPages,
+      results : []
     };
-    result.body.hits.hits.forEach((hit, index) => {
-      const newSource = Object.assign({}, hit._source);
+    result.body.hits.hits.forEach((hit,index) => {
+      const newSource = Object.assign({},hit._source);
       delete newSource.userId;
       response.results.push(newSource);
     });
     res.status(201);
     res.json(response);
     res.end();
-    console.log(`you've got ${result.body.hits.hits.length} matches`)
+    console.log(`you've got ${result.body.hits.hits.length} matches`);
   } catch (error) {
   }
-}
+};
 
 module.exports = retrieveAll;
--- elasticControllers/searchMultiple.js
+++ elasticControllers/searchMultiple.js
@@ -3,42 +3,42 @@ const client = require('../elasticDb');
 
 // ------------------ expected req object start ---------------------------//
 const receivedReq = {
-  body: {
-    searchedText: 'stackoverflow mongoose',
-    pageNum: 1
+  body : {
+    searchedText : 'stackoverflow mongoose',
+    pageNum : 1
   }
-}
+};
 // ------------------ expected req object stop ---------------------------//
 
-const retrieveMultiple = async (req, res) => {
+const retrieveMultiple = async (req,res) => {
   let pageNum = 0;
-  if(req.body.pageNum !== undefined) pageNum = req.body.pageNum - 1;
+  if (req.body.pageNum !== undefined) pageNum = req.body.pageNum - 1;
   const pageSize = 20;
   const searchedText = req.body.searchedText;
   try {
     const result = await client.search({
-      index: 'history',
-      body: {
-        size: pageSize,
-        from: pageNum*pageSize,
-          query: {
-            multi_match : {
-              query : searchedText,
-              fields : [ "pageTitle^3", "pageText", 'url' ]
-            }
+      index : 'history',
+      body : {
+        size : pageSize,
+        from : pageNum * pageSize,
+        query : {
+          multi_match : {
+            query : searchedText,
+            fields : [ 'pageTitle^3','pageText','url' ]
           }
+        }
 
       }
-    })
-    const totalPages = Math.ceil(result.body.hits.total.value/20);
+    });
+    const totalPages = Math.ceil(result.body.hits.total / 20);
     console.log(totalPages);
     const response = {
-      hits: result.body.hits.hits.length,
-      totalPageNum: totalPages,
-      results: []
+      hits : result.body.hits.hits.length,
+      totalPageNum : totalPages,
+      results : []
     };
-    result.body.hits.hits.forEach((hit, index) => {
-      const newSource = Object.assign({}, hit._source);
+    result.body.hits.hits.forEach((hit,index) => {
+      const newSource = Object.assign({},hit._source);
       delete newSource.userId;
       response.results.push(newSource);
     });
@@ -46,11 +46,11 @@ const retrieveMultiple = async (req, res) => {
     res.json(response);
     res.end();
     res.status(201);
-      res.json(result.body.hits.hits);
-      res.end();
-      console.log(`you've got ${result.body.hits.hits.length} matches`)
+    res.json(result.body.hits.hits);
+    res.end();
+    console.log(`you've got ${result.body.hits.hits.length} matches`);
   } catch (error) {
     console.log(error);
   }
-}
+};
 module.exports = retrieveMultiple;

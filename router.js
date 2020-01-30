4f784b1b1e1ce43fca9cf3cdff3875d03456ee42
davstern <hello@davidstern.me>
2020-01-30 11:26:48 +0100

    fix: remove old route and required controller

--- router.js
+++ router.js
@@ -1,14 +1,12 @@
 const router = require ('express').Router();
-const {getAllUrls, createNewUrl, deleteUrl} = require ('./controllers/url');
 const addNewRecord = require('./elasticControllers/addOne');
 const retrieveMultipleTextSearch = require('./elasticControllers/searchMultipleText');
 const retrieveMultipleTitleSearch = require('./elasticControllers/searchMultipleTitle');
-const getAll = require('./elasticControllers/searchAll')
+const getAll = require('./elasticControllers/searchAll');
 
-router.get('', retrieveMultipleTextSearch);
-router.get('/all', getAll);
-router.get('/title', retrieveMultipleTitleSearch);
-router.post('', addNewRecord);
-router.delete('/urls/:id', deleteUrl);
+router.get('',retrieveMultipleTextSearch);
+router.get('/all',getAll);
+router.get('/title',retrieveMultipleTitleSearch);
+router.post('',addNewRecord);
 
 module.exports = router;

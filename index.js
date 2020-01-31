8125b412904280f093e24d79d5d01df994f3d6e0
davstern <hello@davidstern.me>
2020-01-31 19:26:46 +0100

    chore: clear old comments

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 require('dotenv').config({ path : './.env.dev' });
 const express = require ('express');
-// const cors = require ('cors');
 const app = express();
 const router = require ('./router');
 
 const { APP_PORT } = process.env;
 
-// app.use(cors({ origin : 'http://localhost:3000' }));
 app.use(express.json());
 app.use(router);
 

1faf9f0db7134c824ee356752c0ed08bbb31af0d
davstern <hello@davidstern.me>
2020-01-31 19:21:09 +0100

    refactor: eliminate redundancy in controller

--- elasticControllers/searchMultiple.js
+++ elasticControllers/searchMultiple.js
@@ -1,32 +1,6 @@
 'use strict';
 const client = require('../elasticDb');
-var moment = require('moment');
-
-const textExtract = (str,kws,maxlen = 100,tag) =>{
-  console.log('kws : ' + kws);
-  if (!str) return '';
-  if (!kws || !kws.length) {
-    return str.substr(Math.floor(str.length / 2),maxlen);
-  }
-  let strLow = str.toLowerCase();
-  let kwsTolow = kws.toLowerCase();
-  let start = strLow.indexOf(kwsTolow);
-  let end = kws.length;
-  if (start > -1) return `${str.substring(start - 10,start)}<${tag}>${str.substr(start,end)}</${tag}>${str.substring(end,start + maxlen - start)}`;
-  return str.substr(Math.floor(str.length / 2),maxlen);
-};
-
-function highlightKws (str,kwsStr) {
-
-  const termsArr = kwsStr.split(' ');
-  let ret = str;
-  console.log('termsArr:' + termsArr);
-  termsArr.forEach((term)=>{
-    ret = ret.replace(new RegExp(term,'gi'),(match) => `<strong>${match}</strong>`);
-  }
-  );
-  return ret;
-}
+const elasticResToFront = require('../utils/elasticResToFront');
 
 const retrieveMultiple = async (req,res) => {
   let pageNum = 0;
@@ -34,7 +8,7 @@ const retrieveMultiple = async (req,res) => {
   const pageSize = 20;
 
   try {
-    const result = await client.search({
+    res.searchResults = await client.search({
       index : 'history',
       track_scores : true,
 
@@ -55,32 +29,7 @@ const retrieveMultiple = async (req,res) => {
       }
     });
 
-    const totalPages = Math.ceil(result.body.hits.total.value / 20);
-    const response = {
-      hits : result.body.hits.total.value,
-      totalPageNum : totalPages,
-      results : []
-    };
-    result.body.hits.hits.forEach((hit,index) => {
-      const newSource = Object.assign({},hit._source);
-      newSource.shortUrl = newSource.url.length < 40 ? newSource.url : newSource.url.substr(0,40) + '...';
-      newSource.domain = newSource.url
-        .split('/')[0]
-        .split('?')[0];
-      newSource.pageTitle = highlightKws(newSource.pageTitle,req.params.search);
-      newSource.pageText = textExtract(newSource.pageText,req.params.search,100,'strong');
-      newSource.lastVisitTime = moment(newSource.log[newSource.log.length - 1].visitStartTime).fromNow();
-      delete newSource.userId;
-      response.results.push(newSource);
-    });
-    res.set({
-      'Access-Control-Allow-Origin' : '*'
-    })
-      .status(201)
-      .json(response)
-      .end();
-
-    console.log(`you've got ${result.body.hits.hits.length} matches`);
+    return elasticResToFront(req,res);
   } catch (error) {
     console.log(error);
   }

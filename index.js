1d7f6f51d6b9536429b3973056187305bd3f4425
davstern <hello@davidstern.me>
2020-02-02 11:15:46 +0100

    fix: but which caused 413 error response on large post request. set express.json limit to 10mb

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require('dotenv').config({ path : './.env.dev' });
 const express = require ('express');
-const app = express();
 const router = require ('./router');
+const app = express();
 
 const { APP_PORT } = process.env;
 
-app.use(express.json());
+app.use(express.json({limit : '10mb'}));
 app.use(router);
 
 app.listen(APP_PORT,()=> console.log(`Server is running at port ${APP_PORT}`));

e954821264fc9a60d7e7112dd142d00c60899164
davstern <hello@davidstern.me>
2020-02-05 13:39:43 +0100

    fix: add wilcard querying domain

--- elasticControllers/deleteDomain.js
+++ elasticControllers/deleteDomain.js
@@ -1,26 +1,30 @@
 'use strict';
 const esClient = require('../elasticDb');
 
-const deleteDomain = async(req,res) => {
+const deleteDomain = async (req,res) => {
+  console.log('delete domain called');
+  console.log(req.body);
   try {
     const response = await esClient.deleteByQuery({
-      index: 'history-reindexed',
-      body: {
-        query: {
-          bool: {
-            must: [
-              { term: { 'userId.keyword': { value: req.body.userId } } },
-              { term: { 'domain.keyword': { value: req.body.domain } } }
+      index : 'history-reindexed',
+      body : {
+        query : {
+          bool : {
+            must : [
+              { term : { 'userId.keyword' : { value : req.body.userId } } },
+              { wildcard : { 'domain' : { value : req.body.domain } } }
             ],
           }
         }
       }
     });
+    console.log('-----no error---');
     res.status(202).json(response);
     // res.end();
   } catch (error) {
-    console.log(error);
+    console.log('error' + error);
+    res.status(500).json(error);
   }
-}
+};
 
 module.exports = deleteDomain;

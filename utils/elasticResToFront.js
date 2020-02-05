6171f6f527e19393ccef11955735a72a28f6de8e
davstern <hello@davidstern.me>
2020-02-05 13:41:50 +0100

    fix: add userId to return object

--- utils/elasticResToFront.js
+++ utils/elasticResToFront.js
@@ -19,7 +19,7 @@ const elasticResToFront = (req,res) => {
     };
     searchResults.body.hits.hits.forEach((hit) => {
       const newSource = Object.assign({},hit._source);
-      delete newSource.userId;
+      // delete newSource.userId;
       delete newSource.hits;
       newSource.shortUrl = newSource.url.length < 40 ? newSource.url : newSource.url.substr(0,40) + '...';
       newSource.lastVisitTime = moment(newSource.log[newSource.log.length - 1].visitStartTime).fromNow();
@@ -28,7 +28,7 @@ const elasticResToFront = (req,res) => {
       newSource.domain = newSource.url
         .split('/')[0]
         .split('?')[0];
-      delete newSource.userId;
+      // delete newSource.userId;
       // delete newSource.totalPageNum;
       response.results.push(newSource);
     });
